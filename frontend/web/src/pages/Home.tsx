b0a0882255d3ee2e5935471a669e3097408c6d00
JJCUBER <jehjeh1020@gmail.com>
2024-06-22 21:24:39 -0400

    add example of how login/signup popup will look

--- frontend/web/src/pages/Home.tsx
+++ frontend/web/src/pages/Home.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import Modal from "../components/Modal";
+
 export default function Home() {
+    let [isModalOpen, setIsModalOpen] = useState(true);
     return (
         <>
             <p>This is the home page.</p>
+            <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen} shouldCloseOnLostFocus={true}>
+                <>
+                    <h1>The login/signup popup will look like this</h1>
+                    <p>Sample Text.</p>
+                </>
+            </Modal>
         </>
     );
 }
