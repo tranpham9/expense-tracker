4f8ab5cd48582e3fc49bd6f9e8969d8e1ee83be1
JJCUBER <jehjeh1020@gmail.com>
2024-06-25 03:55:59 -0400

    cleanup Modal code

--- frontend/web/src/components/Modal.tsx
+++ frontend/web/src/components/Modal.tsx
@@ -12,7 +12,7 @@ const style = {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    // width: 400,
+    width: "80%",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
@@ -20,20 +20,19 @@ const style = {
 };
 
 export default function TransitionModal({ isOpen, setIsOpen, shouldCloseOnLostFocus = false, children }: { isOpen: boolean, setIsOpen: (newValue: boolean) => void, shouldCloseOnLostFocus?: boolean, children: ReactElement }) {
+    const close = () => {
+        setIsOpen(false);
+    };
+
     const handleClose = () => {
-        console.log(shouldCloseOnLostFocus)
         if (shouldCloseOnLostFocus)
-            setIsOpen(false);
-    }
+            close();
+    };
 
     return (
         <div>
-            {/* <Button onClick={handleOpen}>Open modal</Button> */}
             <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
                 open={isOpen}
-                // This isn't needed since we don't want it to close simply by clickin
                 onClose={handleClose}
                 closeAfterTransition
                 slots={{ backdrop: Backdrop }}
@@ -45,12 +44,6 @@ export default function TransitionModal({ isOpen, setIsOpen, shouldCloseOnLostFo
             >
                 <Fade in={isOpen}>
                     <Box sx={style}>
-                        {/* <Typography id="transition-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography> */}
                         {children}
                     </Box>
                 </Fade>
