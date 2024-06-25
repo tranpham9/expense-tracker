2acd6d2b5a0b746d48301e371266df102b43f02c
John <jtran6796@gmail.com>
2024-06-24 21:26:25 -0400

    login endpoint

--- backend/api/server.js
+++ backend/api/server.js
@@ -20,27 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 // register
 app.post('/api/registerUser', async (req, res, next) =>
     {
-        /*const bodyProperties = Object.keys(req.body);
-        const numFields = bodyProperties.length;*/
 
         const db = client.db('appData');
         const userColl = db.collection('User');
         const tripColl = db.collection('Trip');
 
-
-        //let newUserId = new ObjectId();
-        //var tripId = '';
-
-        // registering with an invitation
-        //if(numFields == 4){
+        // incoming name, email, password, tripId(invitation)
         const {name, email, password, tripId} = req.body;
-        //}
-        // default registering
-        /*else {
-            var {name, email, password} = req.body;
-        }*/
+
         let newUser = {
-            //_id:newUserId,
             name: name,
             email: email,
             password: password,
@@ -49,24 +37,54 @@ app.post('/api/registerUser', async (req, res, next) =>
         try {
             // Update users collection with the new user
             await userColl.insertOne(newUser);
-    
+
+            // query for the _id automatically created by mongodb
+            const query = { email: email};
+            const projection = {_id: 1};
+
+            // retrieve only the _id from the database
+            const result = await userColl.findOne(query, projection)
+
             // If tripId is provided, update trips collection to add user to the trip
-            /*if (tripId) {
-                await tripsCollection.updateOne(
+            if (tripId) {
+                await tripColl.updateOne(
                     { _id: ObjectId(tripId) }, // Find the trip by tripId
-                    { $push: { users: newUserId } } // Add newUserId to the users array in the trip document
+                    { $push: { User: result._id } } // Add newUserId to the users array in the trip document
                 );
-            }*/
+            }
     
             res.status(200).json({ message: 'User registered successfully' });
         } catch (err) {
             console.error('Error registering user:', err);
             res.status(500).json({ error: 'Failed to register user' });
         }
-
-
-
     });
+
+app.post('/api/login', async (req, res, next) =>
+{
+    const db = client.db('appData');
+    const userColl = db.collection('User');
+
+    const {email, password} = req.body;
+
+    const user = await userColl.findOne({email: email});
+
+    if(user){
+        
+        const result = (password === user.password);
+
+        if(result){
+            res.status(200).json({message: user._id});
+        }
+        else{
+            res.status(400).json({error: "Email/Password doesn't match"});
+        }
+    }
+    else{
+        res.status(200).json({error: "User doesn't exist"});
+    }
+
+});
 
 app.listen(5000, () => {
     console.log('listening on port 5000')
