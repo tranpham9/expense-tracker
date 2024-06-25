c8f109141a8605ec093717a70e3db744c0261541
JJCUBER <jehjeh1020@gmail.com>
2024-06-25 05:04:01 -0400

    move login/signup to its own component with tabs

---
+++ frontend/web/src/components/AccountOverlay.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import { Box, Tab, Tabs, TextField } from "@mui/material";
+
+import Modal from "./Modal";
+import Signup from "./Signup";
+import Login from "./Login";
+
+export default function AccountOverlay() {
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const [activeTab, setActiveTab] = useState(0);
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setActiveTab(newValue);
+    };
+
+    return (
+        <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
+            <>
+                <Box sx={{ width: '100%' }}>
+                    <Tabs value={activeTab} onChange={handleTabChange} centered>
+                        <Tab label="Login" />
+                        <Tab label="Signup" />
+                    </Tabs>
+                </Box>
+                <Box sx={{
+                    textAlign: "center",
+                    pt: "10px",
+                }}>
+                    {/* {activeTab ? <Signup /> : <Login />} */}
+                </Box>
+            </>
+        </Modal>
+    );
+}
--- frontend/web/src/pages/Home.tsx
+++ frontend/web/src/pages/Home.tsx
@@ -1,42 +1,11 @@
-import { useState } from "react";
+import AccountOverlay from "../components/AccountOverlay";
 
-import { Box, Container, Tab, Tabs, TextField, Typography } from "@mui/material";
-
-import Modal from "../components/Modal";
 
 export default function Home() {
-    let [isModalOpen, setIsModalOpen] = useState(true);
-    const [value, setValue] = useState(0);
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
     return (
         <>
             <p>This is the home page.</p>
-            <Modal isOpen={isModalOpen} setIsOpen={setIsModalOpen}>
-                <>
-                    <Box sx={{ width: '100%' }}>
-                        <Tabs value={value} onChange={handleChange} centered>
-                            <Tab label="Login" />
-                            <Tab label="Signup" />
-                        </Tabs>
-                    </Box>
-                    <Box sx={{
-                        textAlign: "center",
-                        pt: "10px",
-                    }}>
-                        <TextField variant="outlined" label="email" sx={{
-                            width: "80%",
-                            p: "5px",
-                        }} />
-                        <br />
-                        <TextField variant="outlined" label="password" sx={{
-                            width: "80%",
-                            p: "5px",
-                        }} />
-                    </Box>
-                </>
-            </Modal>
+            <AccountOverlay />
         </>
     );
 }
