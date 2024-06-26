b54afe4a6390205cf62ab92c12102209574d8bf7
JJCUBER <jehjeh1020@gmail.com>
2024-06-26 12:54:07 -0400

    set up name input validation

--- frontend/web/src/components/Signup.tsx
+++ frontend/web/src/components/Signup.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 import { Box, Button, TextField } from "@mui/material";
 
+import NameInput from "./inputs/NameInput";
+
+// TODO: need to make the submit button be disabled if not all fields are valid (it should start disabled since fields don't start as red until the user starts typing in them)
 export default function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signup = () => {
+        console.log(name, email, password);
+        // TODO: impl
+    };
+
     return (
         <Box
             sx={{
@@ -8,12 +22,7 @@ export default function Signup() {
                 pt: "10px",
             }}
         >
-            <TextField
-                variant="outlined"
-                label="Name"
-                sx={{ width: "90%" }}
-                margin="dense"
-            />
+            <NameInput setName={setName} />
             <br />
             <TextField
                 variant="outlined"
@@ -32,6 +41,7 @@ export default function Signup() {
             <Button
                 variant="contained"
                 sx={{ m: "10px" }}
+                onClick={signup}
             >
                 Submit
             </Button>
---
+++ frontend/web/src/components/inputs/NameInput.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps, useState } from "react";
+
+import StyledInput from "./StyledInput";
+
+export default function NameInput({ setName }: { setName: (newName: string) => void }) {
+    const [error, setError] = useState("");
+    const validate: ComponentProps<typeof StyledInput>["onChange"] = (event) => {
+        if (event.target.value.length < 4) {
+            setError("Must be at least 4 characters long");
+            return;
+        }
+
+        if (event.target.value.match(/[^A-Za-z ]/)) {
+            setError("Can only have letters and spaces");
+            return;
+        }
+
+        setError("");
+        setName(event.target.value);
+    };
+    return (
+        <StyledInput
+            label="Name"
+            error={error}
+            onChange={validate}
+        />
+    );
+}
