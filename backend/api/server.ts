a67cbc07c060bf9f583592af2ad075e7ea04b089
JJCUBER <jehjeh1020@gmail.com>
2024-07-01 17:43:27 -0400

    convert server.js to ts ( closes #8 )

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -2,11 +2,11 @@ import "dotenv/config";
 import express, { json, urlencoded } from "express";
 import { join } from "path";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient, ObjectId } from "mongodb";
 
 // Heroku will pass the port we must listen on via the environment, otherwise default to 5000.
 const port = process.env.PORT || 5000;
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI!;
 const client = new MongoClient(uri);
 client.connect();
 
@@ -19,11 +19,27 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
+type User = {
+    _id?: ObjectId;
+    name: string;
+    email: string;
+    password: string;
+    trips: ObjectId[];
+};
+
+type Trip = {
+    _id?: ObjectId;
+    name: string;
+    notes: string;
+    memberIds: ObjectId[];
+    leaderId: ObjectId;
+};
+
 // register
 app.post("/api/registerUser", async (req, res, next) => {
     const db = client.db("appData");
-    const userColl = db.collection("User");
-    const tripColl = db.collection("Trip");
+    const userColl: Collection<User> = db.collection("User");
+    const tripColl: Collection<Trip> = db.collection("Trip");
 
     // incoming name, email, password, tripId(invitation)
 
@@ -31,34 +47,37 @@ app.post("/api/registerUser", async (req, res, next) => {
     if (!name || !email || !password) {
         res.status(400).json({ error: "Bad Request" });
     }
-    let newUser = {
+
+    const newUser: User = {
         name: name,
         email: email,
         password: password,
         trips: [tripId],
     };
     try {
-        const check = await userColl.find(newUser.email);
-        if (check) res.status(500).json({ error: "Failed to register user" });
+        const check = await userColl.find({ email: newUser.email });
+        if (check) {
+            res.status(500).json({ error: "Failed to register user" });
+        }
+
         // Update users collection with the new user
         await userColl.insertOne(newUser);
 
         // query for the _id automatically created by mongodb
-        const query = { email: email };
-        const projection = { _id: 1 };
-
         // retrieve only the _id from the database
-        const result = await userColl.findOne(query, projection);
-        if (result) res.status(200).json({ message: "User registered successfully" });
-        else {
+        const result = await userColl.findOne({ email }, { projection: { _id: true } });
+        if (result) {
+            res.status(200).json({ message: "User registered successfully" });
+        } else {
             res.status(500).json({ error: "Failed to register user" });
         }
 
         // If tripId is provided, update trips collection to add user to the trip
         if (tripId) {
+            // use createFromHexString ( https://github.com/dotansimha/graphql-code-generator/issues/6830#issuecomment-2105266455 )
             await tripColl.updateOne(
-                { _id: ObjectId(tripId) }, // Find the trip by tripId
                 { $push: { User: result._id } } // Add newUserId to the users array in the trip document
+                { _id: ObjectId.createFromHexString(tripId) }, // Find the trip by tripId
             );
         }
     } catch (err) {
