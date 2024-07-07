2d2593ef40fecb3f6112f9cd556025c3ffb9719d
JJCUBER <jehjeh1020@gmail.com>
2024-07-07 01:25:43 -0400

    run prettier format

--- frontend/web/src/pages/Home.tsx
+++ frontend/web/src/pages/Home.tsx
@@ -2,7 +2,6 @@ import AccountOverlay from "../components/AccountOverlay";
 import Hero from "../components/Hero";
 
 export default function Home() {
-    
     return (
         <>
             <AccountOverlay />
