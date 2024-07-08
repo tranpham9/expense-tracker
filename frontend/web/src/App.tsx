68a5c78b4b037ccc2da1e8fc18dc2b89227f93f4
JJCUBER <jehjeh1020@gmail.com>
2024-07-08 13:01:44 -0400

    add TODO

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -5,12 +5,16 @@ import Navbar from "./components/Navbar";
 import Trips from "./pages/Trips";
 
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // TODO: once logging in and JWT is set up, need to handle redirecting from trips to home if not logged in/authenticated
 export default function App() {
     // TODO: this should default to false once logging in is set up
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        // TODO: store/load relevant JWT
+    });
 
     return (
         <BrowserRouter>
