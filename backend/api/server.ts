e3b65cb2f9c4de77b7a0fd6d685ec426915a3290
John <jtran6796@gmail.com>
2024-07-08 17:10:45 -0400

    fixed a syntax error

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -110,6 +110,7 @@ app.post("/api/registerUser", async (req, res, next) => {
     if (!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
             res.status(406).json("Must be a valid email");
             return;
+    }
     // TODO: need to validate email is in valid form and that nothing is too long
     // TODO: ensure trips should also include non-owning trips (i.e. ones where the user is a part of but not the creater/owner of)
     const newUser: User = {
