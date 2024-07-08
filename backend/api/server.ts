b294d8e55580822e367a06791cb123fa9914f509
John <jtran6796@gmail.com>
2024-07-08 15:09:51 -0400

    imported valid email check from frontend to backend

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -64,7 +64,7 @@ app.get("/api/verify/:token", async (req, res, next) => {
                 res.status(401).json({ error: "Failed to register user" });
                 return;
             }
-            res.redirect(HOMEPAGE);
+            res.status(308).redirect(HOMEPAGE);
         }
 
         /*// if tripId is provided, add user to trip
@@ -107,7 +107,9 @@ app.post("/api/registerUser", async (req, res, next) => {
         res.status(400).json({ error: "Malformed Request" });
         return;
     }
-
+    if (!email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            res.status(406).json("Must be a valid email");
+            return;
     // TODO: need to validate email is in valid form and that nothing is too long
     // TODO: ensure trips should also include non-owning trips (i.e. ones where the user is a part of but not the creater/owner of)
     const newUser: User = {
