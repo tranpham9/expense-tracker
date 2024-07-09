d997b4f1893c11a26dd632acc122a6922c3d34cc
Tran Pham <tranpham9@yahoo.com>
2024-07-09 01:08:17 -0400

    changed file names and started about us

--- frontend/web/src/pages/Home.tsx
+++ frontend/web/src/pages/Home.tsx
@@ -1,5 +1,11 @@
-import Hero from "../components/Hero";
+import AboutUs from "../components/AboutUs";
+import Welcome from "../components/Welcome";
 
 export default function Home() {
-    return <Hero />;
+    return (
+        <>
+            <Welcome />
+            <AboutUs />
+        </>
+    );
 }
