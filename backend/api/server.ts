819f2b7c874ddc0b30419ea37eb974984dbf95ce
JJCUBER <34446698+JJCUBER@users.noreply.github.com>
2024-07-09 17:17:31 -0400

    revert incorrect modification to static files path

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -216,7 +216,7 @@ app.use("/api/trips", tripCRUDRouter);
 app.use("/api/expenses", expenseCRUDRouter);
 
 // Serve the static frontend files
-const FRONTEND_DIST_PATH = join(__dirname, "../../../frontend/web"); // starting from dist folder for server.js (compiled from server.ts)
+const FRONTEND_DIST_PATH = join(__dirname, "../../../frontend/web/dist"); // starting from dist folder for server.js (compiled from server.ts)
 app.use(express.static(FRONTEND_DIST_PATH));
 
 // any get request provides the index.html file (which avoids issues with pathing/routing)
