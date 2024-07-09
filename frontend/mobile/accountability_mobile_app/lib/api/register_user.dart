f59a27a0549676c8cd0694f979b3b534b9c18886
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2024-07-09 13:42:51 -0400

    Fixed the URL for verification

--- frontend/mobile/accountability_mobile_app/lib/api/register_user.dart
+++ frontend/mobile/accountability_mobile_app/lib/api/register_user.dart
@@ -1,35 +1,37 @@
 // Needed to make API calls
 import 'dart:convert';
-
 import 'package:accountability_mobile_app/api/config.dart';
 import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
 
 class RegisterUser {
-  static void register(String name, String email, String password) async {
+  static Future<int?> register(
+      String name, String email, String password) async {
     final Dio dio = new Dio();
-
+    int? statusCode = 0;
+    // Attempt to register with the info
     try {
-      var response = await dio.post(
-          '${Config.remoteApiURL}${Config.registerAPI}',
-          data: jsonEncode(<String, String>{
-            "name": name,
-            "email": email,
-            "password": password
-          }),
-          options: Options(
-            contentType: Headers.jsonContentType,
-            responseType: ResponseType.json,
-          ));
-
+      Response response = await dio.post(
+        '${Config.remoteApiURL}${Config.registerAPI}',
+        data: jsonEncode(<String, String>{
+          "name": name,
+          "email": email,
+          "password": password
+        }),
+      );
+      // Display the response code and body
+      print(
+          "Response Code = ${response.statusCode}\nResponse Data:\n${response.data}");
+      // If the response isn't 200, set the status code to the error
       if (response.statusCode != 200) {
+        statusCode = response.statusCode;
         throw Exception("Failed to Register User");
       }
-
-      print(response.statusCode);
-      return;
+      // response was successful
+      statusCode = 200;
+      return statusCode;
     } catch (e) {
       print("Error $e");
+      return statusCode;
     }
   }
 }
