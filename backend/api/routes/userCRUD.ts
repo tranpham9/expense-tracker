710d0a740545cfabc9ad19162c663e2e89558466
JJCUBER <jehjeh1020@gmail.com>
2024-07-14 07:38:48 -0400

    properly wrap user endpoints in try/catch

--- backend/api/routes/userCRUD.ts
+++ backend/api/routes/userCRUD.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { DB_NAME, getMongoClient, STATUS_BAD_REQUEST, STATUS_INTERNAL_SERVER_ERROR, STATUS_OK, STATUS_UNAUTHENTICATED, Trip, TRIP_COLLECTION_NAME, User, USER_COLLECTION_NAME } from "./common";
-import { Collection, ObjectId } from "mongodb";
+import { Collection, MongoClient, ObjectId } from "mongodb";
 import { createEmail, resetPasswordEmail, unverified } from "../tokenSender";
 import { verify } from "jsonwebtoken";
 import md5 from "md5";
@@ -14,10 +14,8 @@ const AUTHENTICATED_ROUTES = ["/changeName", "/joinTrip"];
 router.use(AUTHENTICATED_ROUTES, authenticationRouteHandler);
 
 router.post("/register", async (req, res, next) => {
-    const client = await getMongoClient();
+    let client: MongoClient | undefined;
     try {
-        const db = client.db(DB_NAME);
-        const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
         // TODO: I believe we don't need an optional trip id here anymore?
         const { name, email, password, tripId } = req.body;
         if (!name || !email || !password) {
@@ -37,6 +35,10 @@ router.post("/register", async (req, res, next) => {
             trips: tripId ? [ObjectId.createFromHexString(tripId.toString())] : [],
         };
 
+        client = await getMongoClient();
+        const db = client.db(DB_NAME);
+        const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
+
         // ensure email doesn't already exist
         const check = await userCollection.findOne({ email: email });
         // console.log(check);
@@ -51,15 +53,16 @@ router.post("/register", async (req, res, next) => {
 
         // console.log("A user was registered successfully");
         res.status(STATUS_OK);
-    } catch (err) {}
+    } catch (error) {
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
+    } finally {
+        await client?.close();
+    }
 });
 
 router.post("/login", async (req, res, next) => {
-    const client = await getMongoClient();
+    let client: MongoClient | undefined;
     try {
-        const db = client.db(DB_NAME);
-        const userCollection = db.collection(USER_COLLECTION_NAME);
-
         const { email, password } = req.body;
         if (!email || !password) {
             res.status(STATUS_BAD_REQUEST).json({ error: "Malformed request" });
@@ -68,6 +71,10 @@ router.post("/login", async (req, res, next) => {
 
         // trimmed and converted to lowercase to standardize format of emails
         const properEmail = (email.toString() as string).trim().toLocaleLowerCase();
+
+        client = await getMongoClient();
+        const db = client.db(DB_NAME);
+        const userCollection = db.collection(USER_COLLECTION_NAME);
 
         const foundUser = await userCollection.findOne({ email: properEmail });
         if (foundUser && password === foundUser.password) {
@@ -80,19 +87,17 @@ router.post("/login", async (req, res, next) => {
         } else {
             res.status(STATUS_UNAUTHENTICATED).json({ error: "Invalid login credentials" });
         }
-    } catch (err) {
+    } catch (error) {
         res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
+    } finally {
+        await client?.close();
     }
 });
 
 router.put("/changeName", async (req, res) => {
-    const client = await getMongoClient();
+    let client: MongoClient | undefined;
     try {
-        const db = client.db(DB_NAME);
-        const userCollection = db.collection(USER_COLLECTION_NAME);
-
-        let { newName } = req.body;
-
+        const { newName } = req.body;
         if (!newName) {
             res.status(STATUS_BAD_REQUEST).json({ error: "Malformed request" });
             return;
@@ -104,6 +109,10 @@ router.put("/changeName", async (req, res) => {
             return;
         }
 
+        client = await getMongoClient();
+        const db = client.db(DB_NAME);
+        const userCollection = db.collection(USER_COLLECTION_NAME);
+
         const result = await userCollection.updateOne({ _id: userId }, { $set: { name: newName } });
 
         if (result.modifiedCount === 1) {
@@ -111,52 +120,59 @@ router.put("/changeName", async (req, res) => {
         } else {
             res.status(STATUS_BAD_REQUEST).json({ error: "Failed to update name" });
         }
-    } catch (err) {
+    } catch (error) {
         res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
+    } finally {
+        await client?.close();
     }
 });
 
 router.post("/forgotPassword", async (req, res) => {
-    const { email } = req.body;
-
-    const client = await getMongoClient();
+    let client: MongoClient | undefined;
     try {
+        const { email } = req.body;
+        if (!email) {
+            res.status(STATUS_BAD_REQUEST).json({ error: "Malformed request" });
+            return;
+        }
+
+        client = await getMongoClient();
         const db = client.db(DB_NAME);
         const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
 
         const foundEmail = userCollection.findOne({ email });
         if (!foundEmail) {
-            res.status(STATUS_BAD_REQUEST).send("No email found in the database.");
+            res.status(STATUS_BAD_REQUEST).send("Invalid email");
             return;
         }
 
         await resetPasswordEmail(email);
+
         res.status(STATUS_OK);
     } catch (error) {
         res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
     } finally {
-        await client.close();
+        await client?.close();
     }
 });
 
 // TODO: move to tripCRUD
 router.post("/joinTrip", async (req, res, next) => {
-    const { inviteCode } = req.body;
-
-    // check incoming params
-    if (inviteCode) {
-        res.status(STATUS_BAD_REQUEST).json({ error: "inviteCode required" });
-        return;
-    }
-
-    const userId = extractUserId(res.locals.refreshedJWT);
-    if (!userId) {
-        res.status(STATUS_UNAUTHENTICATED).json({ error: "Malformed JWT" });
-        return;
-    }
-
-    const client = await getMongoClient();
+    let client: MongoClient | undefined;
     try {
+        const { inviteCode } = req.body;
+        if (!inviteCode) {
+            res.status(STATUS_BAD_REQUEST).json({ error: "inviteCode required" });
+            return;
+        }
+
+        const userId = extractUserId(res.locals.refreshedJWT);
+        if (!userId) {
+            res.status(STATUS_UNAUTHENTICATED).json({ error: "Malformed JWT" });
+            return;
+        }
+
+        client = await getMongoClient();
         const db = client.db(DB_NAME);
         const userCol: Collection<User> = db.collection(USER_COLLECTION_NAME);
         const tripCol: Collection<Trip> = db.collection(TRIP_COLLECTION_NAME);
@@ -180,17 +196,14 @@ router.post("/joinTrip", async (req, res, next) => {
     } catch (error) {
         res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
     } finally {
-        await client.close();
+        await client?.close();
     }
 });
 
 // note that, due to how the create JWT code is hardcoded to only work for user login sessions, the authentication middleware can't be used for this route/endpoint; accordingly, the jwt gets checked with isExpired
 router.post("/resetPassword", async (req, res) => {
-    const client = await getMongoClient();
+    let client: MongoClient | undefined;
     try {
-        const db = client.db(DB_NAME);
-        const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
-
         const { newPassword, jwt } = req.body;
         if (!newPassword || !jwt) {
             res.status(STATUS_BAD_REQUEST).json({ error: "Malformed request" });
@@ -208,6 +221,10 @@ router.post("/resetPassword", async (req, res) => {
             res.status(STATUS_UNAUTHENTICATED).json({ error: "Malformed JWT" });
             return;
         }
+
+        client = await getMongoClient();
+        const db = client.db(DB_NAME);
+        const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
 
         const user = await userCollection.findOne({ _id: userId });
         if (!user) {
@@ -229,67 +246,79 @@ router.post("/resetPassword", async (req, res) => {
     } catch (error) {
         res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
     } finally {
-        await client.close();
+        await client?.close();
     }
 });
 
 router.get("/verify/:token", async (req, res) => {
-    const client = await getMongoClient();
-    const db = client.db(DB_NAME);
-    const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
-    const tripCollection: Collection<Trip> = db.collection(TRIP_COLLECTION_NAME);
-
-    // get User document from token
-    const verified = unverified.get(req.params.token);
-    console.log(req.params.token);
-
+    let client: MongoClient | undefined;
     try {
-        if (verified) {
-            // insert new user
-            const insertionResult = await userCollection.insertOne(verified);
-            if (!insertionResult.acknowledged) {
-                console.error("Failed to insert new user");
-                res.status(STATUS_UNAUTHENTICATED).json({ error: "Failed to register user" });
-                return;
-            }
-            // If we get here, we're successfully verified
-            // Remove from unverified list to prevent double registration
-            unverified.delete(req.params.token);
-            /*
+        // get User document from token
+        const { token } = req.params;
+        const verified = unverified.get(token);
+        if (!verified) {
+            res.status(STATUS_BAD_REQUEST).send(`
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+        <h3>Verification failed; invalid verification code.</h3>
+    </body>
+</html>
+`);
+            return;
+        }
+
+        client = await getMongoClient();
+        const db = client.db(DB_NAME);
+        const userCollection: Collection<User> = db.collection(USER_COLLECTION_NAME);
+        const tripCollection: Collection<Trip> = db.collection(TRIP_COLLECTION_NAME);
+
+        // insert new user
+        const result = await userCollection.insertOne(verified);
+        if (!result.acknowledged) {
+            res.status(STATUS_BAD_REQUEST).send(`
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+    <body>
+        <h3>Failed to register user.</h3>
+    </body>
+</html>
+`);
+            return;
+        }
+        // If we get here, we're successfully verified
+        // Remove from unverified list to prevent double registration
+        unverified.delete(req.params.token);
+        /*
             // Redirect to the application's homepage (static files root)
             res.status(308).redirect('/');
             */
-            // Show some basic HTML that redirects to homepage after 5 seconds
-            res.status(STATUS_OK).send(`
-                <html>
-                <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta http-equiv="refresh" content="5;url=/" />
-                </head>
-                <body>
-                <h3>Verification successful. Please return to your app or main website. Redirecting in 5 seconds.</h3>
-                </body>
-                </html>
-            `);
-            return; // Ensure no further code executes
+        // Show some basic HTML that redirects to homepage after 5 seconds
+        res.status(STATUS_OK).send(`
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="refresh" content="5;url=/" />
+    </head>
+    <body>
+        <h3>Verification successful. Redirecting to website in 5 seconds.</h3>
+    </body>
+</html>
+        `);
 
-            /*// if tripId is provided, add user to trip
+        /*// if tripId is provided, add user to trip
             if (verified?.trips) {
                 // use createFromHexString ( https://github.com/dotansimha/graphql-code-generator/issues/6830#issuecomment-2105266455 )
                 // TODO: might need to handle this failing?  Would need to check .acknowledged boolean
                 await tripCollection.updateOne({ _id: ObjectId.createFromHexString(tripId) }, { $push: { memberIds: insertionResult.insertedId } });
             }*/
-        }
-    } catch (err) {
-        console.error("Error registering user:", err);
-        if (!res.headersSent) {
-            res.status(STATUS_UNAUTHENTICATED).json({ error: "Failed to register user" });
-        }
-        return;
-    }
-
-    // We get here if verification wasn't successful
-    if (!res.headersSent) {
-        res.status(STATUS_UNAUTHENTICATED).json({ error: "Invalid registration token" });
+    } catch (error) {
+        res.status(STATUS_INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
+    } finally {
+        await client?.close();
     }
 });
