7e7e7e355e9ea6a04b9714e6a2a53a00dc20705b
Jacob Gadberry <jagadberry03@gmail.com>
2024-07-19 01:11:06 -0400

    JWT Fixed Return Problem
    'expenses/get' wasn't returning a 'jwt', there was a mix up with the return jwt function call.

---
+++ frontend/mobile/accountability_mobile_app/lib/api/expense_crud.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'package:accountability_mobile_app/api/config.dart';
+import 'package:accountability_mobile_app/globals.dart';
+import 'package:dio/dio.dart';
+import '../models/Expense.dart';
+
+class ExpenseCRUD {}
