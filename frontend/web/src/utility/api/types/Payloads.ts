b8a25925cf223eb9ace67e0bcbc7385292c6f9b1
JJCUBER <jehjeh1020@gmail.com>
2024-07-19 10:26:06 -0400

    add payload + response types for /api/users/* (web)

--- frontend/web/src/utility/api/types/Payloads.ts
+++ frontend/web/src/utility/api/types/Payloads.ts
@@ -2,13 +2,24 @@ type UsersRegisterPayload = {
     name: string;
     email: string;
     password: string;
+    bio?: string;
 };
 type UsersLoginPayload = {
     email: string;
     password: string;
 };
+type UsersUpdatePayload = {
+    name?: string;
+    bio?: string;
+    // isAuthenticatedRoute: true;
+};
+type UsersForgotPasswordPayload = { email: string };
+type UsersResetPasswordPayload = { newPassword: string };
 
 export type Payloads = {
     "users/register": UsersRegisterPayload;
     "users/login": UsersLoginPayload;
+    "users/update": UsersUpdatePayload;
+    "users/forgotPassword": UsersForgotPasswordPayload;
+    "users/resetPassword": UsersResetPasswordPayload;
 };
--- frontend/web/src/utility/api/types/Responses.ts
+++ frontend/web/src/utility/api/types/Responses.ts
@@ -1,14 +1,18 @@
-type UsersRegisterResponse = {
-    message: string;
-};
+type UsersRegisterResponse = { message: string };
 export type UsersLoginResponse = {
     userId: string;
     name: string;
     email: string;
-    jwt: string;
+    jwt: string; // rest of the responses don't need to specify this explicitly
 };
+type UsersUpdateResponse = object; // empty response (when ignoring jwt)
+type UsersForgotPasswordPayload = { message: string };
+type UsersResetPasswordPayload = { message: string };
 
 export type Responses = {
     "users/register": UsersRegisterResponse;
     "users/login": UsersLoginResponse;
+    "users/update": UsersUpdateResponse;
+    "users/forgotPassword": UsersForgotPasswordPayload;
+    "users/resetPassword": UsersResetPasswordPayload;
 };
