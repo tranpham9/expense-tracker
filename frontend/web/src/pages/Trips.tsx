65c8e8040c4a519e8de5cf25e3d3b83b363328ae
JJCUBER <jehjeh1020@gmail.com>
2024-07-19 20:27:54 -0400

    impl fetch and show trips (web)

--- frontend/web/src/pages/Trips.tsx
+++ frontend/web/src/pages/Trips.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../components/inputs/SearchBar";
-import { Box, Grid, Pagination, Paper, Skeleton, Stack } from "@mui/material";
+import { Box, Grid, Pagination, Paper, Skeleton, Stack, Typography } from "@mui/material";
 import { isLoggedIn } from "../Signals/Account";
-import { useSignals } from "@preact/signals-react/runtime";
+import { useSignal, useSignalEffect, useSignals } from "@preact/signals-react/runtime";
+import { request } from "../utility/api/API";
+import { Trip } from "../utility/api/types/Responses";
 
 export default function Trips() {
     useSignals();
@@ -29,17 +31,40 @@ export default function Trips() {
         });
     }, []);
     */
+    const trips = useSignal<Trip[] | null>(null);
 
     useEffect(() => {
         if (!isLoggedIn.value) {
             console.log("<not logged in>");
             navigate("/home");
+            return;
         }
+    });
+
+    useSignalEffect(() => {
+        if (isLoggedIn.value) {
+            console.log("Just became logged in; running signal effect in Trips");
+
+            request(
+                "trips/search",
+                {},
+                (response) => {
+                    trips.value = response.trips;
+                },
+                (errorMessage) => {
+                    console.trace("uhoh", errorMessage);
+                }
+            );
+        }
+    });
+
+    useSignalEffect(() => {
+        console.log("Trips changed", trips);
     });
 
     // TODO: extract this to its own file once pagination isproperly set up in api
     const [page, setPage] = useState(1);
-    const LinkedPagination = () => (
+    const LinkedPagination = ({ isEnabled = true }) => (
         <Box
             sx={{
                 m: 1,
@@ -52,9 +77,99 @@ export default function Trips() {
                 count={10}
                 color="primary"
                 page={page}
+                disabled={!isEnabled}
                 onChange={(_event, page) => setPage(page)}
             />
         </Box>
+    );
+
+    const LoadingSkeleton = () => (
+        <Stack sx={{ textAlign: "center" }}>
+            {[...Array(10).keys()].map((i) => (
+                <Paper
+                    key={i}
+                    elevation={10}
+                    sx={{
+                        m: 1,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Grid
+                        container
+                        p={2}
+                        spacing={2}
+                    >
+                        <Grid
+                            item
+                            xs={2}
+                        >
+                            <Skeleton width="100%" />
+                            <Skeleton width="100%" />
+                        </Grid>
+                        <Grid
+                            item
+                            xs={8}
+                        >
+                            <Skeleton width="100%" />
+                            <Skeleton width="100%" />
+                        </Grid>
+                        <Grid
+                            item
+                            xs={2}
+                        >
+                            <Skeleton width="100%" />
+                            <Skeleton width="100%" />
+                        </Grid>
+                    </Grid>
+                </Paper>
+            ))}
+        </Stack>
+    );
+
+    const RenderedTrips = () => (
+        <Stack sx={{ textAlign: "center" }}>
+            {trips.value?.map((trip, i) => (
+                <Paper
+                    key={i}
+                    elevation={10}
+                    sx={{
+                        m: 1,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Grid
+                        container
+                        p={2}
+                        spacing={2}
+                        alignItems="center"
+                    >
+                        <Grid
+                            item
+                            xs={2}
+                        >
+                            <Typography>{trip.name}</Typography>
+                        </Grid>
+                        <Grid
+                            item
+                            xs={8}
+                        >
+                            {trip.description}
+                        </Grid>
+                        <Grid
+                            item
+                            xs={2}
+                        >
+                            <Skeleton width="100%" />
+                            <Skeleton width="100%" />
+                        </Grid>
+                    </Grid>
+                </Paper>
+            ))}
+        </Stack>
     );
 
     return (
@@ -69,50 +184,9 @@ export default function Trips() {
             >
                 <SearchBar />
             </Box>
-            <LinkedPagination />
-            <Stack sx={{ textAlign: "center" }}>
-                {[...Array(10).keys()].map((i) => (
-                    <Paper
-                        key={i}
-                        elevation={10}
-                        sx={{
-                            m: 1,
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Grid
-                            container
-                            p={2}
-                            spacing={2}
-                        >
-                            <Grid
-                                item
-                                xs={2}
-                            >
-                                <Skeleton width="100%" />
-                                <Skeleton width="100%" />
-                            </Grid>
-                            <Grid
-                                item
-                                xs={8}
-                            >
-                                <Skeleton width="100%" />
-                                <Skeleton width="100%" />
-                            </Grid>
-                            <Grid
-                                item
-                                xs={2}
-                            >
-                                <Skeleton width="100%" />
-                                <Skeleton width="100%" />
-                            </Grid>
-                        </Grid>
-                    </Paper>
-                ))}
-                <LinkedPagination />
-            </Stack>
+            {(!trips.value || trips.value.length) && <LinkedPagination isEnabled={!!trips.value} />}
+            {trips.value ? <RenderedTrips /> : <LoadingSkeleton />}
+            <LinkedPagination isEnabled={!!trips.value} />
         </>
     );
 }
