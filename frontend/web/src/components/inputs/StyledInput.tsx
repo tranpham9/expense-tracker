d7ad22dd9dd77b475d8e572a42c7d5d4d020037a
JJCUBER <jehjeh1020@gmail.com>
2024-07-23 22:19:23 -0400

    support two-way binding ("controlled") style input

--- frontend/web/src/components/inputs/StyledInput.tsx
+++ frontend/web/src/components/inputs/StyledInput.tsx
@@ -6,6 +6,7 @@ export default function StyledInput({
     type = "text",
     label = "",
     error = "",
+    value,
     required = true,
     useMultiline = false,
     // These have no default values on purpose!
@@ -16,6 +17,11 @@ export default function StyledInput({
     type?: HTMLInputTypeAttribute;
     label?: string;
     error?: string;
+    // NOTE: this should only really be used for when state needs to flow into it as well (kind of like a two-way binding; this is called "controlled" in react + MUI:
+    //     https://mui.com/material-ui/react-text-field/#uncontrolled-vs-controlled
+    //     https://react.dev/learn/sharing-state-between-components#controlled-and-uncontrolled-components
+    // )
+    value?: string;
     required?: boolean;
     useMultiline?: boolean;
     onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
@@ -33,6 +39,7 @@ export default function StyledInput({
             required={required}
             error={error !== ""}
             helperText={error}
+            value={value}
             // This only shows when there is no error
             // FIXME: this also shows right at the start, which isn't desired
             // color="success"
